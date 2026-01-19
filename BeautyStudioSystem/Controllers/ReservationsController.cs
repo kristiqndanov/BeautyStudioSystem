8ea78ad8a5b89311630e0cc1f311027b46caa6cf
kristiqn.danov <kristiqn.danov@gmail.com>
2026-01-19 15:59:54 +0200

    Added ReservationsController, ReservationsService, view for Reservations/Index.cshtml

---
+++ BeautyStudioSystem/Controllers/ReservationsController.cs
@@ -0,0 +1,22 @@
+﻿using BeautyStudioSystem.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautyStudioSystem.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private readonly IReservationsService _reservationsService;
+
+        public ReservationsController(IReservationsService reservationsService)
+        {
+            _reservationsService = reservationsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var reservationsViewModels = _reservationsService.GetAllReservationsAsync();
+
+            return View(reservationsViewModels);
+        }
+    }
+}
---
+++ BeautyStudioSystem/Services/ReservationsService.cs
@@ -0,0 +1,41 @@
+﻿using BeautyStudioSystem.Infrastructure.Contracts;
+using BeautyStudioSystem.Services.Contracts;
+using BeautyStudioSystem.ViewModels;
+
+namespace BeautyStudioSystem.Services
+{
+    public class ReservationsService : IReservationsService
+    {
+        private readonly IReservationsRepository _reservationsRepository;
+
+        public ReservationsService(IReservationsRepository reservationsRepository)
+        {
+            _reservationsRepository = reservationsRepository;
+        }
+
+
+        public async Task<IEnumerable<ReservationViewModel>> GetAllReservationsAsync()
+        {
+            var reservationViewModels = new List<ReservationViewModel>();
+
+            var allReservations = await _reservationsRepository.GetAllAsync();
+
+            foreach (var reservation in allReservations)
+            {
+                var reservationViewModel = new ReservationViewModel
+                {
+                    Id = reservation.Id,
+                    Date = reservation.Date.ToShortDateString(),
+                    ClientName = $"{reservation.Client.FirstName} {reservation.Client.LastName}",
+                    ServiceName = $"{reservation.Service.Name}",
+                    StartTime = reservation.StartTime.ToShortTimeString()
+                };
+
+                reservationViewModels.Add(reservationViewModel);
+            }
+
+            return reservationViewModels;
+        }
+
+    }
+}
