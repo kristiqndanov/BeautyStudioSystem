c8abc78c6021d0da58555e9a28f511983f444bec
kristiqn.danov <kristiqn.danov@gmail.com>
2026-03-15 13:38:17 +0200

    Added unit test for DeleteServiceAsync in ServicesService.

--- BeautyStudioSystem.Tests/ServicesServiceTests.cs
+++ BeautyStudioSystem.Tests/ServicesServiceTests.cs
@@ -26,10 +26,10 @@ namespace BeautyStudioSystem.Tests
         [Test]
         public async Task AddServiceAsync_ShouldThrowArgumentNullException_WhenServiceViewModelIsNull()
         {
-            
+
             ServiceViewModel serviceViewModel = null;
 
-            
+
             Assert.ThrowsAsync<ArgumentNullException>(async () => await _servicesService.AddServiceAsync(serviceViewModel));
         }
 
@@ -48,7 +48,7 @@ namespace BeautyStudioSystem.Tests
         [Test]
         public async Task AddServiceAsync_WhenAllValid_ShouldAddService()
         {
-            
+
             var serviceViewModel = new ServiceViewModel
             {
                 Name = "Haircut",
@@ -57,10 +57,23 @@ namespace BeautyStudioSystem.Tests
                 ServiceCategoryId = 1
             };
 
-            
+
             await _servicesService.AddServiceAsync(serviceViewModel);
 
             _servicesRepositoryMock.Verify(repo => repo.AddServiceAsync(It.IsAny<Service>()), Times.Once);
+        }
+
+        [Test]
+        public async Task DeleteServiceAsync_ShouldDeleteService()
+        {
+            int serviceId = 1;
+            var service = new Service { Id = serviceId };
+
+            _servicesRepositoryMock.Setup(repo => repo.GetByIdAsync(serviceId)).ReturnsAsync(service);
+
+            await _servicesService.DeleteServiceAsync(serviceId);
+
+            _servicesRepositoryMock.Verify(repo => repo.DeleteServiceAsync(service), Times.Once);
         }
     }
 }
